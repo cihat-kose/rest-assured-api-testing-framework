4a5428649669ba3ac54b95df010d11915a1117e6
ChatGTHB <cihat.kose@hotmail.com>
2023-11-02 21:09:37 +0100

    Added ZippoTest.java

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,35 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ZippoTest {
+
+    @Test
+    public void test01() {
+
+        given()
+                // Preparatory procedures codes
+
+                .when()
+                // Endpoint (url), giving the method and sending the request
+
+                .then()
+               // Assertion, test, data operations
+        ;
+    }
+
+    @Test
+    public void statusCodeTest() {
+
+        given()
+                // Preparation section is empty
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body() // Returning body json data, log().all(): everything going back and forth
+                .statusCode(200) // Is the assertion status code 200?
+        ;
+    }
+}

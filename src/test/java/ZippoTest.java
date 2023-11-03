15a4712698fe75e884305bb55890acc9c384fbcf
ChatGTHB <cihat.kose@hotmail.com>
2023-11-03 20:17:22 +0100

    Added setup() and requestResponseSpecification() methods to ZippoTest.java

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,6 +1,13 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -234,5 +241,38 @@ public class ZippoTest {
                     .body("meta.pagination.page", equalTo(i))
             ;
         }
+    }
+
+    RequestSpecification requestSpec;
+    ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void setup(){
+        baseURI = "https://gorest.co.in/public/v1";
+
+        requestSpec= new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.URI)  // log().uri()
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)  // statusCode(200)
+                .log(LogDetail.BODY)  //log().body()
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    @Test
+    public void requestResponseSpecification(){
+        given()
+                .param("page",1)
+                .spec(requestSpec)
+
+                .when()
+                .get("/users") // http hok ise baseUri baş tarafına gelir.
+
+                .then()
+                .spec(responseSpec)
+        ;
     }
 }

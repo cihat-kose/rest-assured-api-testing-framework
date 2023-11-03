128cc3968eca8cdafbf5319b54b03c414f2b1ab7
ChatGTHB <cihat.kose@hotmail.com>
2023-11-03 23:10:07 +0100

    Added extractingJSONPath2() method to _02_APITestExtract.java

--- src/test/java/_02_APITestExtract.java
+++ src/test/java/_02_APITestExtract.java
@@ -28,6 +28,35 @@ public class _02_APITestExtract {
     }
 
 
+    @Test
+    public void extractingJSONPath2() {
+        /**
+         * Soru : "http://api.zippopotam.us/us/90210"  endpoint inden dönen place dizisinin ilk elemanının
+         * state değerinin "California" olduğunu testNG Assertion ile doğrulayınız
+         * ------------------------------------------------------------------------------------------------------------
+         * Question: The first element of the place array returned from the endpoint "http://api.zippopotam.us/us/90210"
+         * verify that the state value is "California" with testNG Assertion
+         */
+
+
+        String stateName =
+
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+
+                .then()
+                .log().body()
+                .extract().path("places[0].state")
+
+        ;
+
+        System.out.println("State name = " + stateName);
+        Assert.assertEquals(stateName, "California");
+    }
+
 
 
 }

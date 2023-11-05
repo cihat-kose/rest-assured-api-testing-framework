0b33f146efaa7074598104fad63696b010f8c2b6
ChatGTHB <cihat.kose@hotmail.com>
2023-11-05 11:04:52 +0100

    Updated _03_APITestPOJO.java, Location.java and Place.java

--- src/test/java/_03_APITestPOJO.java
+++ src/test/java/_03_APITestPOJO.java
@@ -1,2 +1,29 @@
-public class _03_APITestPOJO {
+import model.Location;
+import model.Place;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class _03_APITestPOJO { // POJO = JSON Object
+    @Test
+    public void extractJSONAll() {
+
+        Location locationObject =
+
+                given()
+
+                        .when()
+                        .get("http://api.zippopotam.us/us/90210")
+
+                        .then()
+                        .extract().body().as(Location.class);
+
+        System.out.println("locationObject = " + locationObject);
+        System.out.println("locationObject.getCountry() = " + locationObject.getCountry());
+        System.out.println("locationObject.getPlaces() = " + locationObject.getPlaces());
+
+        for (Place place : locationObject.getPlaces()) {
+            System.out.println("place = " + place);
+        }
+    }
 }

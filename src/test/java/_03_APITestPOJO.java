673ead151e3780f74e94f5e05a5d07b268feb8af
ChatGTHB <cihat.kose@hotmail.com>
2023-11-05 11:49:42 +0100

    Updated _03_APITestPOJO.java

--- src/test/java/_03_APITestPOJO.java
+++ src/test/java/_03_APITestPOJO.java
@@ -6,7 +6,7 @@ import static io.restassured.RestAssured.*;
 
 public class _03_APITestPOJO { // POJO = JSON Object
     @Test
-    public void extractJSONAll() {
+    public void extractJsonAll_POJO() {
 
         Location locationObject =
 
@@ -26,4 +26,28 @@ public class _03_APITestPOJO { // POJO = JSON Object
             System.out.println("place = " + place);
         }
     }
+
+    @Test
+    public void extractPOJO_Question() {
+
+        Location locationObject =
+
+                given()
+
+                        .when()
+                        .get("http://api.zippopotam.us/tr/01000")
+
+                        .then()
+                        .extract().body().as(Location.class)
+                ;
+
+        for(Place place:locationObject.getPlaces()){
+            if(place.getPlacename().equalsIgnoreCase("Dörtağaç Köyü")){
+                System.out.println("place = " + place);
+            }
+        }
+        System.out.println("locationObject.getPlaces().get(2) = " + locationObject.getPlaces().get(2));
+    }
+
+    // http://api.zippopotam.us/tr/01000  endpointinden dönen verilerden "Dörtağaç Köyü" ait bilgileri yazdırınız
 }

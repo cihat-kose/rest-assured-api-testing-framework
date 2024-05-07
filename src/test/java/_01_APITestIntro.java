5878ac19d0eb13047c5eda68b63d100483114ae5
ChatGTHB <cihat.kose@hotmail.com>
2024-05-08 00:31:47 +0200

    Edited

--- src/test/java/_01_APITestIntro.java
+++ src/test/java/_01_APITestIntro.java
@@ -1,13 +1,6 @@
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
-import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
